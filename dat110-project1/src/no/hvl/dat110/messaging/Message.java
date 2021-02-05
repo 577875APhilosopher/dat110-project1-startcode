0a503ab3208939b5cece006f609a356a4a182767
577875APhilosopher <577875APhilosopher@users.noreply.github.com>
2021-02-05 13:56:40 +0100

    Oppdatert message

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -9,7 +9,9 @@ public class Message {
 	private byte[] payload;
 
 	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
+		if (payload.length<MessageConfig.SEGMENTSIZE) {
+		this.payload = payload;
+		}
 	}
 
 	public Message() {
@@ -22,26 +24,25 @@ public class Message {
 
 	public byte[] encapsulate() {
 		
-		byte[] encoded = null;
+		byte[] encoded = new byte[MessageConfig.SEGMENTSIZE];
+		encoded[0]=(byte) payload.length;
 		
-		// TODO
-		// encapulate/encode the payload of this message in the
-		// encoded byte array according to message format
-		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
-
+		for (int i =0; i<payload.length; i++) {
+			encoded[i+1]=payload[i];
+		}
 		return encoded;
 		
 	}
 
 	public void decapsulate(byte[] received) {
-
-		// TODO
-		// decapsulate the data contained in the received byte array and store it 
-		// in the payload of this message
 		
-		throw new UnsupportedOperationException(TODO.method());
+		int length = received[0];
+		this.payload = new byte[length];
+		
+		for (int i = 0; i<length; i++) {
+			this.payload[i]=received[i+1];
+		}
+		
 		
 	}
 }
